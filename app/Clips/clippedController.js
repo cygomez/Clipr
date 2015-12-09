aeddc29e5e3fb420390319bea4deb0316f02693d
Cryus Gomez Alcala <cyrus.gomeza@gmail.com>
2015-12-09 14:43:56 -0800

    [fix]: remove image that display when render facebook button

--- app/Clips/clippedController.js
+++ app/Clips/clippedController.js
@@ -137,6 +137,7 @@ angular.module('clipr.clipped', ['ui.router', 'ui.bootstrap', 'ngAside'])
 
 }]);
 
+
 var ModalInstanceCtrl = function($scope, $modalInstance, Clips, $modal, item) {
   $scope.collections = Clips.clips.collections
   $scope.item = item.clip
