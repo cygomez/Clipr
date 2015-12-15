046063b07b01d0f2b197af98f77695e7894aa2d9
minseokim <minseoalexkim@gmail.com>
2015-12-15 15:42:33 -0800

    [chore]: fix image on readme

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-// LOAD DEPENDENCIES
+//Load dependencies
 var compression = require('compression');
 var express = require('express');
 var path = require('path');
@@ -6,12 +6,11 @@ var bodyParser = require('body-parser');
 var Promise = require('bluebird');
 var request = require('request');
 var http = require('http');
-// var router = require('./router.js');
 var cookieParser = require('cookie-parser');
 var classifier= require('./config/classify.js')
 var urlToImage = require('url-to-image');
 
-// INITIALIZE SERVER
+// Initialize server
 var port = process.env.PORT || 3000;
 var app = module.exports= express();
 var routes = require('./router.js');
@@ -20,6 +19,5 @@ var routes = require('./router.js');
 app.use(compression());
 app.use(express.static(__dirname + '../../app/dist'));
 
-
 app.listen(port);
-console.log('Bits please server is now running at ' + port);
+console.log('Clippr server is now running at ' + port);
